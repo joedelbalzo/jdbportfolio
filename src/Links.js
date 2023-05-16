7e9f56f701aa16df628cd92eb46a7461555040a9
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-05-16 19:53:02 -0400

    new links

--- src/Links.js
+++ src/Links.js
@@ -5,8 +5,8 @@ const Links = ()=> {
     <div id="linkPage">
       <div id="links">
         <a href="https://www.linkedin.com/in/joe-delbalzo/" id="link" target="_blank" rel="noreferrer noopener">LinkedIn &nbsp;&nbsp;&nbsp; &#8702;</a>
-        {/* <a href="https://www.linkedin.com/in/joe-delbalzo/" id="link" target="_blank" rel="noreferrer noopener">Portfolio &nbsp;&nbsp;&nbsp; &#8702;</a>
-        <a href="mailto:jdelbalzo99@gmail.com" id="link" target="_blank" rel="noreferrer noopener">Email Me &nbsp;&nbsp;&nbsp; &#8702;</a> */}
+         <a href="https://jdb-open-places.onrender.com/#/" id="link" target="_blank" rel="noreferrer noopener">Portfolio: "Open Places" &nbsp;&nbsp;&nbsp; &#8702;</a>
+        <a href="https://scriptforjava.onrender.com" id="link" target="_blank" rel="noreferrer noopener">Portfolio: "Script for Java" &nbsp;&nbsp;&nbsp; &#8702;</a>
 
       </div>
     </div>
