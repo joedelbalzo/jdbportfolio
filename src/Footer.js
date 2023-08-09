473fa098b8501617b163d8f526bb9e8665adf4f6
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-08-09 09:48:43 -0400

    styles and foot

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div
+        style={{
+          fontSize: "calc(6px + .5vw)",
+          color: "rgb(200,200,200)",
+          paddingTop: "2rem",
+          paddingBottom: "1rem",
+        }}
+      >
+        <p>&copy; Joe Del Balzo, 2023</p>
+      </div>
+    </>
+  );
+};
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Sublinks from "./Sublinks";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -11,32 +12,13 @@ const Home = () => {
         </span>
       </h1>
       <div id="main-introParagraph">
-        <p>Software Engineer and Full Stack Web Developer in New York, NY.</p>
-        <p>Former Lighting Designer, Lighting Console Programmer.</p>
-        <p>
-          Musician, Cold Brew Drinker, Sports Fanatic,{" "}
-          <a
-            href="https://www.cicerone.org/int-en/users/joseph-del-balzo"
-            id="main-name"
-            target="_blank"
-            style={{ textDecoration: "none", color: "rgba(240, 240, 240, 0.9)" }}
-          >
-            Certified Cicerone®
-          </a>
-        </p>
-        <p>
-          <a
-            href="https://www.imdb.com/name/nm3631957/"
-            id="main-name"
-            target="_blank"
-            style={{ textDecoration: "none", color: "rgba(240, 240, 240, 0.9)" }}
-          >
-            Artist-turned-coder.
-          </a>{" "}
-          I liked creating beauty before; I like creating beauty now.
-        </p>
+        <p>Software Engineer and Full Stack Web Developer</p>
+        <p>Former Lighting Designer, Network Designer, and Console Programmer.</p>
+        <p>Musician, Cold Brew Drinker, Sports Fanatic, Certified Cicerone®</p>
+        <p>Artist-turned-coder. I liked creating beauty before; I like creating beauty now.</p>
       </div>
       <Sublinks />
+      <Footer />
     </>
   );
 };
