116dbfb39f61e5e069a24ea6b09827c8771cdff4
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-03-18 10:12:08 -0400

    migration

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const svgLoader = require("svg-inline-loader");
+// const path = require("path");
+// const svgLoader = require("svg-inline-loader");
 
-module.exports = {
-  devtool: "source-map",
-  module: {
-    rules: [
-      { test: /\.svg$/, loader: "svg-inline-loader" },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react"],
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-};
+// module.exports = {
+//   devtool: "source-map",
+//   module: {
+//     rules: [
+//       { test: /\.svg$/, loader: "svg-inline-loader" },
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader",
+//         options: {
+//           presets: ["@babel/preset-react"],
+//         },
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif)$/i,
+//         use: [
+//           {
+//             loader: "file-loader",
+//             options: {
+//               name: "[name].[ext]",
+//               outputPath: "images/",
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".js", ".jsx"],
+//   },
+// };
