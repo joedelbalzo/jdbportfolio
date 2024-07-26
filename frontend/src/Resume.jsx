ce4edc30f62a9cbe37124194ff024764d554114c
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-07-26 11:49:31 -0400

    resume updates

--- frontend/src/Resume.jsx
+++ frontend/src/Resume.jsx
@@ -23,85 +23,114 @@ const Resume = () => {
               <strong>Languages, Frameworks, & Databases: </strong>
               <br />
               <br />
-              JavaScript (ES6+), Typescript, Python, React, Redux, Node.js, Express, Django, PostgreSQL, Sequelize, Axios, Mocha, Jest,
-              Material UI, Framer Motion <br /> <br />
+              JavaScript, TypeScript, Python, React, Redux, Node, Express, PostgreSQL, Sequelize, Axios, Mocha, Jest <br /> <br />
               <strong>Practices, Methodologies, & Technologies: </strong>
               <br />
               <br />
-              RESTful API, Test Driven Development, Git Version Control, Agile (Scrum), AWS S3, Google OAuth2.0 and APIs, Shopify,
-              Squarespace, Google Analytics
+              RESTful API, Test Driven Development, Git Version Control, Agile (Scrum), AWS S3, Shopify, Squarespace, Google OAuth2.0,
+              Google Analytics, and Google APIs
             </p>
           </div>
           <div id="borderdiv">
             <h2>Work Experience</h2>
-            <h3 style={{ marginBottom: "10px" }}>the future of agency, LLC</h3>
-            <a href="https://www.thefutureofagency.com/">https://www.thefutureofagency.com/</a>
-            <h3 style={{ marginTop: "10px" }}>Software Engineer</h3>
+            <h3 style={{ marginBottom: "10px" }}>
+              The Future of Agency &mdash;{" "}
+              <a href="https://www.thefutureofagency.com/" style={{}}>
+                thefutureofagency.com
+              </a>
+            </h3>
+
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              A marketing agency in the book publishing industry.
+            </h3>
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              <i>Software Engineer, October 2023 – Present</i>
+            </h3>
             <ul>
               <li>Leading the development and management of web applications to market and advertise books.</li>
               <li>
-                Collaborating with clients to enhance design and SEO performance, substantially improving unique visitor traffic and Google
-                Search traffic across our portfolio over 400%.
+                Collaborating with clients to enhance design and SEO, increasing unique visitor and Google Search traffic by over 400%.
               </li>
               <li>
-                Engineering and embedding custom full-stack applications on client-side web development platforms, utilizing technologies
-                such as React.js, Node.js, RESTful APIs, reCAPTCHA, to provide our clients more tailored solutions, contributing to enhanced
-                client satisfaction and retention.
+                Engineering and integrating full-stack applications using React.js, Node.js, and RESTful APIs to provide our clients more
+                tailored solutions, enhancing client satisfaction and retention.
               </li>
               <li>
-                Enhancing UI/UX and accessibility with CSS and JavaScript on websites like{" "}
-                <a href="https://www.melodybeattie.com/">melodybeattie.com</a>,{" "}
-                <a href="https://www.benjaminherold.com/">benjaminherold.com</a>, and{" "}
-                <a href="https://www.umanaidoomd.com/">umanaidoomd.com</a>.
+                Improving UI/UX and accessibility on our portfolio of client websites using CSS and JavaScript, enhancing user engagement.
               </li>
               <li>
-                Optimizing DNS configurations and email delivery systems, ensuring robust and reliable communication for mailing lists up to
-                50,000 subscribers.
+                Optimizing DNS configurations and email systems, ensuring reliable communication for client mailing lists upwards of 50,000
+                subscribers.
               </li>
               <li>
                 Implementing company-wide version control for custom designs in Squarespace and Shopify by maintaining a comprehensive
-                version history on GitHub, minimizing down time by 70% with easy code recoverability, smoother updates, and troubleshooting.
+                version history on GitHub, reducing downtime by 70%.
               </li>
             </ul>
-            <h3 style={{ marginBottom: "10px" }}>Vali</h3>
-            <a href="https://www.joinvali.com/">https://www.joinvali.com/</a>
-            <h3 style={{ marginTop: "10px" }}>Founding Software Engineer</h3>
+            <h3 style={{ marginBottom: "10px" }}>
+              Vali &mdash;{" "}
+              <a href="https://www.joinvali.com/" style={{}}>
+                joinvali.com
+              </a>
+            </h3>
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              MVP-stage startup in the financial spectrum.
+            </h3>
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              <i>Full Stack Engineer, July 2023 – November 2023 (Contract)</i>
+            </h3>
             <ul>
-              <li>Designer, architect of an MVP-stage startup, increasing valuation accuracy for small businesses.</li>
-              <li>Implemented AWS S3 and Google OAuth2.0 to increase privacy for data uploads and user sessions.</li>
+              <li>
+                Led the design, architecture, and development of a full stack application of an MVP-stage startup for small business
+                financial valuations to accelerate startup growth.
+              </li>
+              <li>Integrated AWS S3, streamlining content management and upload processes.</li>
+              <li>Leveraged Framer Motion to develop a fluid and responsive user interface, enhancing user experience.</li>
               <li>Tech Used: React, React-Redux, Node.js, Express, PostgreSQL, AWS S3, Google OAuth2.0</li>
             </ul>
             <h3 style={{ marginBottom: "10px" }}>Entertainment Partners & Cast and Crew</h3>
-            <h3 style={{ marginTop: "10px" }}>Console Programmer and Designer</h3>
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              Notable productions: John Wick 3, East New York (CBS), Bull (CBS), and Ray Donovan: The Movie.
+            </h3>
+            <h3 style={{ margin: "4px auto 4px 0", fontWeight: "300", fontSize: "smaller" }}>
+              <i>Lighting Console Programmer and Designer, March 2018 – April 2023</i>
+            </h3>
             <ul>
-              <li>Programming lighting consoles and build robust data networks for film and television productions.</li>
+              <li>Programmed lighting consoles and built data networks for film and television.</li>
               <li>
-                Leading productions from antiquated lighting into modern, LED lighting, with huge increases in efficiency. Individual
-                projects were completed 17% more quickly and energy efficiency increased 30%.
+                Transitioned film and TV sets from outdated incandescent systems to modern LED lighting, cutting project times by 17% and
+                boosting energy efficiency by 30%.
               </li>
-              <li>
-                Communicating regularly in teams of 10 to 15 technicians as well as with high-profile clients and executive producers to
-                efficiently purchase and implement newer lighting technologies, faster networks (both wired and wireless), and deliver
-                critical software and hardware updates to optimize performance and reliability.
-              </li>
-              <li>Notable productions: John Wick 3, East New York Season 1, Bull Seasons 4-6, and Ray Donovan: The Movie.</li>
+              <li>Coordinated new tech installations, optimizing performance and saving over 75 person-hours weekly.</li>
             </ul>
           </div>
           <div id="borderdiv">
             <h2>Freelance and Open Source Projects </h2>
-            <h3>Dawn Ryan, Freelancer</h3>
-            <a href="https://www.dawngryan.com/">https://www.dawngryan.com/</a>
-            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>
-              Front-end Designer and Developer || React, React-Redux, Node.js, Express
-            </div>
+            <h3 style={{ marginBottom: "10px" }}>
+              Dawn Ryan, Freelancer &mdash; <a href="https://www.dawngryan.com/">dawngryan.com</a>
+            </h3>
+            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>Front-End Engineer</div>
             <ul>
               <li>Designed and developed a portfolio and advertising website for a client freelance writer and copy-editor.</li>
               <li>Utilized React, React-Router, and Framer Motion to allow for a seamless user experience.</li>
               <li>Implemented Formspree’s library of front-end forms for end-user and client communication.</li>
             </ul>
-            <h3>“Drop of CSS”</h3>
+            <h3 style={{ marginBottom: "10px" }}>
+              “AlgoRhythm” &mdash; <a href="https://algorhythm-joedelbalzo.vercel.app/">algorhythm-joedelbalzo.vercel.app</a>
+            </h3>
+            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>Full Stack Engineer</div>
+            <ul>
+              <li>
+                Developed a quiz-style application designed to improve a user’s knowledge of common algorithms and solving styles and
+                patterns in JavaScript and Python.
+              </li>
+              <li>Built a rich database with hundreds of quiz questions delivered to the front-end via Axios API fetches.</li>
+            </ul>
+            <h3>
+              “Drop of CSS” &mdash; <a href="./dropofcss">./dropofcss</a>
+            </h3>
             <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>
-              Front-end Designer and Developer || React, React-Redux, Node.js, Express, PostgreSQL
+              Front-End Developer || React, React-Redux, Node.js, Express, PostgreSQL
             </div>
             <ul>
               <li>
@@ -117,23 +146,10 @@ const Resume = () => {
                 guides for enhanced codebase consistency and maintainability.
               </li>
             </ul>
-            <h3 style={{ marginTop: "4rem" }}>“AlgoRhythm”</h3>
-            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>
-              Front-end Designer and Developer || Next.js, Node.js, Express, PostgreSQL
-            </div>
-            Quiz application for JavaScript and Python algorithm studying.
-            <ul>
-              <li>
-                Developed a quiz-style application designed to improve a user’s knowledge of common algorithms and solving styles and
-                patterns in JavaScript and Python.
-              </li>
-
-              <li>Built a rich database with hundreds of quiz questions delivered to the front-end via Axios API fetches.</li>
-            </ul>
-            <h3 style={{ marginTop: "4rem" }}>“Open Places”</h3>
-            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>
-              Front-end Designer and Developer || React, React-Redux, Node.js, Express, PostgreSQL
-            </div>
+            <h3 style={{ marginTop: "4rem" }}>
+              “Open Places” &mdash; <a href="./openplaces/login">./openplaces/login</a>
+            </h3>
+            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>Full Stack Engineer</div>
             <ul>
               <li>
                 Developed a location-based map search application utilizing the Google Places API, React, and Material-UI to ensure
@@ -144,10 +160,10 @@ const Resume = () => {
                 eliminating API costs while in development.
               </li>
             </ul>
-            <h3 style={{ marginTop: "4rem" }}>E-Commerce “A Script for Java”</h3>
-            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>
-              Front-end Designer and Developer || React, React-Redux, Node.js, Express, PostgreSQL
-            </div>
+            <h3 style={{ marginTop: "4rem" }}>
+              E-Commerce “A Script for Java” &mdash; <a href="./scriptforjava">./scriptforjava</a>
+            </h3>
+            <div style={{ fontSize: "calc(8px + 1vw)", lineHeight: "1rem", fontStyle: "italic" }}>Full Stack Engineer</div>
             <ul>
               <li>Designed and built an e-commerce template for small chains of cafes.</li>
               <li>Built administrative tools, authorizing a cafe manager full CRUD capabilities.</li>
